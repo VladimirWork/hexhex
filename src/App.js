dda3b6eb8d0bf138cc415c035ebe27411bdd2982
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2020-10-25 16:15:50 +0300

    add initial hex app
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from 'react'
+import { GridGenerator, HexGrid, Layout, Path, Hexagon, Text, Pattern, Hex } from 'react-hexgrid'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+  const hexagonSize = { x: 5, y: 5 }
+  const hexes1 = GridGenerator.hexagon(2)
+  const hexes2 = GridGenerator.triangle(2)
+  const hexes3 = GridGenerator.rectangle(2, 2)
+    return (
+      <div className="App">
+        <HexGrid width={1800} height={900}>
+          <Layout size={hexagonSize} spacing={1.05} origin={{ x: 0, y: 0 }}>
+            { hexes1.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          </Layout>
+
+          <Layout size={hexagonSize} spacing={1.05} origin={{ x: -50, y: 0 }}>
+            { hexes2.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          </Layout>
+
+          <Layout size={hexagonSize} spacing={1.05} origin={{ x: 50, y: 0 }}>
+            { hexes3.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          </Layout>
+        </HexGrid>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
