e6bac4df76ee0ed5a03a02aff0c71a436dad8ce5
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2020-10-26 22:38:50 +0300

    playing with hexes
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ class App extends Component {
   render() {
   const hexagonSize = { x: 5, y: 5 }
   const hexes1 = GridGenerator.hexagon(3)
-  const hexes2 = GridGenerator.triangle(3)
-  const hexes3 = GridGenerator.rectangle(3, 3)
-  const hexes4 = GridGenerator.ring(new Hex(0, 0, 0), 3)
-  const hexes5 = GridGenerator.spiral(new Hex(1, 1, 1), 5)
+  const hexes11 = GridGenerator.hexagon(1)
+  // const hexes2 = GridGenerator.triangle(3)
+  // const hexes3 = GridGenerator.orientedRectangle(3, 3)
+  // const hexes4 = GridGenerator.ring(new Hex(0, 0, 0), 3)
+  // const hexes5 = GridGenerator.spiral(new Hex(1, 1, 1), 5)
     return (
       <div className="App">
         <HexGrid width={1900} height={950}>
@@ -17,12 +18,12 @@ class App extends Component {
             { hexes1.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
           </Layout>
 
-          <Layout size={hexagonSize} spacing={1.05} origin={{ x: -75, y: 0 }}>
-            { hexes2.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          <Layout size={hexagonSize} spacing={1.05} origin={{ x: -50, y: 25 }}>
+            { hexes11.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
           </Layout>
 
-          <Layout size={hexagonSize} spacing={1.05} origin={{ x: 75, y: 0 }}>
-            { hexes3.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          <Layout size={hexagonSize} spacing={1.05} origin={{ x: 50, y: -25 }}>
+            { hexes11.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
           </Layout>
         </HexGrid>
       </div>
