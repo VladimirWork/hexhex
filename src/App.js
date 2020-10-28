0acf2c2afebebb876d7e5eb376c96d5d0ec66a18
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2020-10-28 23:21:49 +0300

    update
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   const hexes1 = GridGenerator.hexagon(3)
   const hexes11 = GridGenerator.hexagon(1)
   // const hexes2 = GridGenerator.triangle(3)
-  // const hexes3 = GridGenerator.orientedRectangle(3, 3)
+  const hexes3 = GridGenerator.orientedRectangle(1, 5)
   // const hexes4 = GridGenerator.ring(new Hex(0, 0, 0), 3)
   // const hexes5 = GridGenerator.spiral(new Hex(1, 1, 1), 5)
     return (
@@ -26,6 +26,14 @@ class App extends Component {
           <Layout size={hexagonSize} spacing={1.05} origin={{ x: 50, y: -25 }}>
             { hexes11.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
           </Layout>
+
+          <Layout size={hexagonSize} spacing={1.05} origin={{ x: -90, y: -30 }}>
+            { hexes3.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          </Layout>
+
+          {/* <Layout size={hexagonSize} spacing={1.05} origin={{ x: 50, y: 0 }}>
+            { hexes3.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
+          </Layout> */}
 
           {/* <Pattern id="pat-1" link="https://static.wikia.nocookie.net/diablo_gamepedia/images/b/bf/Blood_Raven_%28Diablo_II%29.gif/revision/latest?cb=20091104210614&format=original" size={patternSize} />
           <Pattern id="pat-2" link="https://static.wikia.nocookie.net/diablo_gamepedia/images/f/fb/Coldcrow_%28Diablo_II%29.gif/revision/latest?cb=20091104202835&format=original" size={patternSize} /> */}
--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,16 @@ import { render, screen } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 import App from './App'
 
-test('renders app', () => {
-  render(<App />)
+describe('renders app and matches snapshot', () => {
 
-  const tree = renderer
-    .create(<App />)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+  test('renders app', () => {
+    render(<App />)
+  })
+
+  test('matches snapshot', () => {
+    const tree = renderer
+      .create(<App />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
