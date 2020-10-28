8ae684a83e232df241920a0520decb691d64d59a
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2020-10-28 22:15:35 +0300

    update
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { GridGenerator, HexGrid, Layout, Path, Hexagon, Text, Pattern, Hex } fro
 class App extends Component {
   render() {
   const hexagonSize = { x: 5, y: 5 }
+  const patternSize = { x: 4, y: 4 }
   const hexes1 = GridGenerator.hexagon(3)
   const hexes11 = GridGenerator.hexagon(1)
   // const hexes2 = GridGenerator.triangle(3)
@@ -25,6 +26,10 @@ class App extends Component {
           <Layout size={hexagonSize} spacing={1.05} origin={{ x: 50, y: -25 }}>
             { hexes11.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />) }
           </Layout>
+
+          {/* <Pattern id="pat-1" link="https://static.wikia.nocookie.net/diablo_gamepedia/images/b/bf/Blood_Raven_%28Diablo_II%29.gif/revision/latest?cb=20091104210614&format=original" size={patternSize} />
+          <Pattern id="pat-2" link="https://static.wikia.nocookie.net/diablo_gamepedia/images/f/fb/Coldcrow_%28Diablo_II%29.gif/revision/latest?cb=20091104202835&format=original" size={patternSize} /> */}
+
         </HexGrid>
       </div>
     )
