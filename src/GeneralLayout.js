7d850201c2c72c9fa698bc41e45324c9ebecaf02
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2020-11-15 13:23:16 +0300

    add new icon and implement drag and drop
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- src/GeneralLayout.js
+++ src/GeneralLayout.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { GridGenerator, Layout, Hexagon, Text, Pattern, HexUtils } from 'react-hexgrid'
 import './GeneralLayout.css'
+import logo from './images/logo192.png'
+import swords from './images/BTN_action047.png'
 
 class GeneralLayout extends Component {
   constructor(props) {
     super(props)
     // Initialize hexagons with some text and image
-    const hexagons = GridGenerator.hexagon(3)
+    const hexagons = GridGenerator.hexagon(3).map((hexagon, index) => {
+      return Object.assign({}, hexagon, {
+        // text: index,
+        image: swords
+      })
+    })
     this.state = { hexagons }
   }
 
@@ -15,11 +22,28 @@ class GeneralLayout extends Component {
   }
 
   onDragEnd(event, source, success) {
-      
+    if (!success) {
+      return
+    }
+
+    const { hexagons } = this.state
+
+    // TODO Drop the whole hex from array, currently somethings wrong with the patterns
+    // const hexas = hexagons.filter(hex => !HexUtils.equals(targetHex, hex));
+    const hexas = hexagons.map(hex => {
+      if (HexUtils.equals(source.state.hex, hex)) {
+        // hex.text = null
+        hex.image = null
+      }
+      return hex
+    })
+
+    this.setState({ hexagons: hexas })
   }
 
   render() {
       const { hexagons } = this.state
+      const patternSize = { x: 6, y: 6 }
       return(
         <Layout className="tiles" size={{ x: 7, y: 7 }} flat={false} spacing={1.05} origin={{ x: 50, y: 0 }}>
             {
@@ -34,7 +58,7 @@ class GeneralLayout extends Component {
                     onDragStart={(e, h) => this.onDragStart(e, h)}
                     onDragEnd={(e, h, s) => this.onDragEnd(e, h, s)}
                 >
-                    { !!hex.image && <Pattern id={HexUtils.getID(hex)} link={hex.image} /> }
+                    { !!hex.image && <Pattern id={HexUtils.getID(hex)} link={hex.image} size={patternSize} /> }
                 </Hexagon>
             ))
             }
--- src/Team2Layout.js
+++ src/Team2Layout.js
@@ -10,15 +10,62 @@ class Team2Layout extends Component {
   }
 
   onDragStart(event, source) {
-
+    // If this tile is empty, let's disallow drag
+    if (!source.props.data.image) {
+      event.preventDefault()
+    }
   }
 
   onDragEnd(event, source, success) {
-      
+    if (!success) {
+      return
+    }
+    const { hexagons } = this.state
+    // When hexagon is successfully dropped, empty it's text and image
+    const hexas = hexagons.map(hex => {
+      if (HexUtils.equals(source.state.hex, hex)) {
+        // hex.text = null
+        hex.image = null
+      }
+      return hex
+    })
+    this.setState({ hexagons: hexas })
+  }
+
+  // onDrop you can read information of the hexagon that initiated the drag
+  onDrop(event, source, targetProps) {
+    const { hexagons } = this.state
+    const hexas = hexagons.map(hex => {
+      // When hexagon is dropped on this hexagon, copy it's image and text
+      if (HexUtils.equals(source.state.hex, hex)) {
+        hex.image = targetProps.data.image
+        // hex.text = targetProps.data.text
+      }
+      return hex
+    })
+    this.setState({ hexagons: hexas })
+  }
+
+  // Decide here if you want to allow drop to this node
+  onDragOver(event, source) {
+    // Find blocked hexagons by their 'blocked' attribute
+    const blockedHexas = this.state.hexagons.filter(h => h.blocked)
+    // Find if this hexagon is listed in blocked ones
+    const blocked = blockedHexas.find(blockedHex => {
+      return HexUtils.equals(source.state.hex, blockedHex)
+    })
+
+    const { image } = source.props.data
+    // Allow drop, if not blocked and there's no content already
+    if (!blocked && !image) {
+      // Call preventDefault if you want to allow drop
+      event.preventDefault()
+    }
   }
 
   render() {
       let { hexagons } = this.state
+      const patternSize = { x: 6, y: 6 }
       return(
         <Layout className="teams" size={{ x: 7, y: 7 }} flat={false} spacing={1.05} origin={{ x: -50, y: 25 }}>
             {
@@ -32,8 +79,10 @@ class Team2Layout extends Component {
                     data={hex}
                     onDragStart={(e, h) => this.onDragStart(e, h)}
                     onDragEnd={(e, h, s) => this.onDragEnd(e, h, s)}
+                    onDrop={(e, h, t) => this.onDrop(e, h, t) }
+                    onDragOver={(e, h) => this.onDragOver(e, h) }
                 >
-                    { !!hex.image && <Pattern id={HexUtils.getID(hex)} link={hex.image} /> }
+                    { !!hex.image && <Pattern id={HexUtils.getID(hex)} link={hex.image} size={patternSize} /> }
                 </Hexagon>
             ))
             }
